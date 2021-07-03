855ff6a5d7f3830105483e79804e12b7d21ac5f6
Minh Phan <trongminh20@gmail.com>
2021-07-02 18:00:32 -0700

    done desktop layout

--- mainJS.js
+++ mainJS.js
@@ -1,1 +1,8 @@
 function ddMenu(){}
+function setBackground(){
+  let divClasses = [...document.getElementsByClassName('creation')];
+  divClasses.forEach((e)=>{
+    let id = e.id;
+    e.style = "background-image: url(images/desktop/"+id+".jpg)";
+  })
+}
