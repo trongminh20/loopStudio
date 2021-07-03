bd1f895e3d4171081c90a7e2ba18e0a1ea3a5b89
Minh Phan <trongminh20@gmail.com>
2021-07-02 22:00:07 -0700

    done with responsive

--- mainJS.js
+++ mainJS.js
@@ -1,8 +1,20 @@
-function ddMenu(){}
+function ddMenu(ele){
+  if(ele.style.display == "flex"){
+    ele.style.display = "none";
+    ele.parentElement.classList.remove("responsive-menu");
+
+  }else{
+    ele.style.display = "flex";
+  }
+}
 function setBackground(){
   let divClasses = [...document.getElementsByClassName('creation')];
   divClasses.forEach((e)=>{
     let id = e.id;
+    if(window.innerWidth >= 800){
     e.style = "background-image: url(images/desktop/"+id+".jpg)";
+    }else if(window.innerWidth <800){
+      e.style = "background-image: url(images/mobile/"+id+".jpg)";
+    }
   })
 }
